616752784d6926352a9133355dd5ccd5aa80cf88
Jherf-Jagonio <jurafu6@gmail.com>
2026-05-02 20:55:23 +0800

    Phase 1 Homework01 - Calculator

--- hw01Calculator.py
+++ hw01Calculator.py
@@ -1,1 +1,21 @@
-print ("Hello World!")
+print("------------------------------------------")
+print("          Building A Calculator           ")
+print("------------------------------------------")
+print("Choosing the following are : (+)  (-) , (*) , (/) ")
+
+choose = input("Type of Operators : ")
+num1 = float((input("Enter a num1: ")))
+num2 = float((input("Enter a num2: ")))
+
+if(choose == '+'):
+    result = num1 + num2
+elif (choose == '-'):
+    result = num1 - num2
+elif (choose == '*'):
+    result = num1 * num2
+elif (choose == '/'):
+    result = float(f"{num1:.2f}") / float(f"{num2:.2f}")
+
+print(f"Result: {result:.2f}")
+
+
